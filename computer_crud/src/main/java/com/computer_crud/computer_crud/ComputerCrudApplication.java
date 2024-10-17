e10f6616b611dcda50eed4cb0db343023e9c981a
enesvarim <114951133+enesvarim@users.noreply.github.com>
2024-10-17 09:54:02 +0300

    Add files via upload

---
+++ computer_crud/src/main/java/com/computer_crud/computer_crud/ComputerCrudApplication.java
@@ -0,0 +1,13 @@
+package com.computer_crud.computer_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerCrudApplication.class, args);
+	}
+
+}
---
+++ computer_crud/src/test/java/com/computer_crud/computer_crud/ComputerCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.computer_crud.computer_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
