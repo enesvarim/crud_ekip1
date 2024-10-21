a6e9c089debb459f4a999553a554d56d21624d0a
enesvarim <114951133+enesvarim@users.noreply.github.com>
2024-10-22 02:47:29 +0300

    initial commit

---
+++ computer_crud/src/main/java/com/computer_crud/computer_crud/DTO/PcNameDTO.java
@@ -0,0 +1,18 @@
+package com.computer_crud.computer_crud.DTO;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PcNameDTO {
+
+    @JsonIgnore
+    private int id;
+
+    private String pcName;
+    private String processorName;
+    private String graphicscardName;
+
+}
